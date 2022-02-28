0686370d33ae0b9c04d158df3d55c99292f3174e
Lowe Silvan <lowesilvan@gmail.com>
2022-02-28 19:14:02 +0100

    Mobile Responsive Styling

--- src/Components/Home/Navbar.js
+++ src/Components/Home/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand mx-3" href="/"><span className="dot align-top mr-3">do</span>Lowe Mbonda</a>
+            <a className="navbar-brand mx-3" href="/Portfolio"><span className="dot align-top mr-3">do</span>Lowe Mbonda</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#toggleMobileMenu" aria-controls="#toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon "></span>
             </button>
