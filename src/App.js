c539e92d1456224eb583ca90956ae620e84bcf63
Lowe Silvan <lowesilvan@gmail.com>
2022-02-28 18:56:09 +0100

    gh pages landing page route

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/Portfolio" element={<Profile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
